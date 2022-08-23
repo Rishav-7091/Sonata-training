e65300379ad54c1b7494839ececfe93c3a1c09eb
Rishav <rishav.rk@sonata-software.com>
2022-08-23 17:27:58 +0530

    final

---
+++ src/test/java/com/sonata/test/EmpTest.java
@@ -0,0 +1,35 @@
+package com.sonata.test;
+
+import com.sonata.Impl.EmpImpl;
+import com.sonataModel.Employee;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EmpTest {
+	Employee e1 = new Employee();
+	EmpImpl e2 = new EmpImpl();
+	@Test
+	public void ytest() {
+		e1.setEmpSal(1000);
+		assertEquals(12000,e2.yearlySal(e1));
+	}
+	@Test
+	public void ytest1() {
+		e1.setEmpSal(100);
+		assertEquals(1200,e2.yearlySal(e1));
+	}
+	@Test
+	public void ytest2() {
+		e1.setEmpSal(1000);
+		assertEquals(5000,e2.appSal(e1));
+	}
+	@Test
+	public void ytest3() {
+		e1.setEmpSal(1000);
+		assertEquals(5000,e2.appSal(e1));
+	}
+
+
+}
