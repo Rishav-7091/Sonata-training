011ed39a2eb0a4cd5421611ed71716eb252fe212
Rishav <rishav.rk@sonata-software.com>
2022-08-24 17:27:29 +0530

    cal V2

--- src/test/java/com/example/demo/SonatatrainingApplicationTests.java
+++ src/test/java/com/example/demo/SonatatrainingApplicationTests.java
@@ -1,13 +1,29 @@
  package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sonata.Cal.Calculator;
 
 @SpringBootTest
 class SonatatrainingApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void divTest() {
+		Calculator c3 = new Calculator();
+		int x = c3.div(10, 10);
+		assertThrows(ArithmeticException.class,()->c3.div(10,0),"it is error");
+	}
+	//@Test
+	public void divTest1() {
+		Calculator c3 = new Calculator();
+		int x = c3.div(10, 10);
+		assertThrows(ArithmeticException.class,()->c3.div(10,10),"it is error");
 	}
 
 }
